85c7d71615f8a92953910d7c2f2a421d7753be8b
Alexandr <42836987+Aschlock@users.noreply.github.com>
2025-03-22 13:03:55 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,11 @@ import React, { Component } from "react";
 
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
 }
-
-export default App;
